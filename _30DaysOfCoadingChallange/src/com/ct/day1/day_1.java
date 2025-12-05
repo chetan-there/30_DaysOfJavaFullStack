de4525be8077c54ebd7fdb5d1e3af13888485969
chetan-there <therechetan24@gmail.com>
2025-12-05 08:30:32 +0530

    intial

---
+++ _30DaysOfCoadingChallange/src/com/ct/day1/day_1.java
@@ -0,0 +1,5 @@
+package com.ct.day1;
+//topic : Variables, Data Types, Control Flow, Functions, OOP concepts.
+public class day_1 {
+
+}
---
+++ _30DaysOfCoadingChallange/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module _30DaysOfCoadingChallange {
+}
